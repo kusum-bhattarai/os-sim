8eb89dec437330777ec5c8e6d062c7a4fd283c10
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2026-04-18 13:14:47 -0500

    feat: add LRU header file, using list+hashmap for efficiency

---
+++ src/policy/lru.h
@@ -0,0 +1,15 @@
+#include "replacement_policy.h"
+#include <unordered_map>
+#include <list>
+
+class LRU: public ReplacementPolicy {
+private:
+    std::list<int> order;  // front = LRU, back = MRU
+    std::unordered_map<int, std::list<int>::iterator> frame_positions;  // frame_index -> position in the list
+
+public:
+    void on_load(int frame_index, int vpn) override;
+    void on_access(int frame_index) override;
+    int evict() override;
+    void reset() override;
+};
