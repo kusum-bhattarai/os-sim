d8cae3ce65e3a0af04b1ee6fe46c890fe3891077
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2026-04-18 13:06:57 -0500

    feat: add FIFO header file

---
+++ src/policy/fifo.h
@@ -0,0 +1,13 @@
+#include "replacement_policy.h"
+#include <queue>
+
+class FIFO: public ReplacementPolicy {
+private:
+    std::queue<int> load_order; // tracks the order of loaded frames
+
+public:
+    void on_load(int frame_index, int vpn) override;
+    void on_access(int frame_index) override;
+    int evict() override;
+    void reset() override;
+};
