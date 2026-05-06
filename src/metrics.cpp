45a5b9c311a704db4bfc36400e4b63d4752cd27f
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2026-05-05 23:22:38 -0500

    feat: updated metrics reset and print with tlb metrics

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -2,12 +2,17 @@
 #include <iostream>
 
 void Metrics::print() const {
-    std::cout << "Page Faults:     " << page_faults      << "\n"
-              << "Hits:            " << hits              << "\n"
-              << "Evictions:       " << evictions         << "\n"
-              << "CoW Forks:       " << cow_forks         << "\n"
-              << "CoW Copies:      " << cow_copies        << "\n"
-              << "Copies Avoided:  " << cow_copies_avoided << "\n";
+    int total_tlb = tlb_hits + tlb_misses;
+    double tlb_hit_rate = total_tlb > 0 ? 100.0 * tlb_hits / total_tlb : 0.0;
+    std::cout << "Page Faults:     " << page_faults       << "\n"
+              << "Hits:            " << hits               << "\n"
+              << "Evictions:       " << evictions          << "\n"
+              << "CoW Forks:       " << cow_forks          << "\n"
+              << "CoW Copies:      " << cow_copies         << "\n"
+              << "Copies Avoided:  " << cow_copies_avoided << "\n"
+              << "TLB Hits:        " << tlb_hits           << "\n"
+              << "TLB Misses:      " << tlb_misses         << "\n"
+              << "TLB Hit Rate:    " << tlb_hit_rate       << "%\n";
 }
 
 void Metrics::reset() {
@@ -17,6 +22,8 @@ void Metrics::reset() {
     cow_forks = 0;
     cow_copies = 0;
     cow_copies_avoided = 0;
+    tlb_hits = 0;
+    tlb_misses = 0;
 }
 
 const Metrics& Metrics::get_metrics() const {
