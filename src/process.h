da42a9262552c663ce490cb4c77215103ad1aac6
Kusum Bhattarai Sharma <xcm15@txstate.edu>
2026-05-06 11:07:40 -0500

    feat: update process constructor

--- src/process.h
+++ src/process.h
@@ -9,7 +9,7 @@ private:
     int pid;  //process ID
 
 public:
-    Process(int pid): pid(pid){}
+    Process(int pid, size_t tlb_size = TLB_SIZE): pid(pid), tlb(tlb_size) {}
     int get_pid() const {return pid;}
     PageTable& get_page_table() {return page_table;}
     TLB& get_tlb() { return tlb; }
